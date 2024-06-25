3bc3a5ccf61f2682d8456105d662740a7b77adf8
OMCHOKSI108 <134709654+OMCHOKSI108@users.noreply.github.com>
2024-06-25 13:46:09 +0530

    Create daycode1.java

---
+++ daycode1.java
@@ -0,0 +1,30 @@
+class mydemo
+{
+   public static void main(String args[])
+ {
+
+  int a=10,b11=105,years=24;;
+  String name="cspit";
+  char c1='A';
+  double d1=8500000;
+  float f1 = (float)8.76;
+  byte b1 =55;
+  boolean d = false;
+  int f = c1;
+  double d2= years;
+  char c3 = (char)b11;
+      //System.out.print("Hello World ");
+      //System.out.print("HEloo 2");
+      //System.out.printf("Strinhg");
+      //System.out.println(a);
+      //System.out.println(b);
+      //System.out.println(a + " " + b);
+      //System.out.println(a + b);
+      //System.out.println(name + " is " + years + " old .");
+      //System.out.println(a + " " + b + " "+ name + " "+ d1 + " " + f1 + " " + b1 +" " + d );
+       // System.out.println("data from command line " + args[0] + args.length);
+       System.out.println(f + " " + d2 + " " + c3);
+
+ } 
+
+}
