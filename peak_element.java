799f2279d4536d6bfdef473c6ab99053a3e2fa58
OM CHOKSI <omchoksi108@gmail.com>
2024-08-10 09:57:30 +0530

    added a function that finds and return smallest element in array and call in main and print

--- peak_element.java
+++ peak_element.java
@@ -16,9 +16,23 @@ public class peak_element {
     return largest;
 
    }
+
+   public static int smallest(int number[])
+   {
+    int smallest = Integer.MAX_VALUE;     //+(infinity)
+    for(int i=0;i<number.length;i++)
+    {
+        if(smallest > number[i])
+        {
+            smallest = number[i];
+        }
+    }
+    return smallest;
+   }
     public static void main(String[] args) {
         int numbers[] = {4,9,32,20,19};
         System.out.println("Largest element is " + largest(numbers));
+        System.err.println("Smallest element is " + smallest(numbers));
  
     }
 }
