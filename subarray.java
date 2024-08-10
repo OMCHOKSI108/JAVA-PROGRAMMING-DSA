8b3877c2f0adbcb2de52f4b82618cd21d582ed02
OM CHOKSI <omchoksi108@gmail.com>
2024-08-10 13:18:14 +0530

    total subarray print and passed a commnet that what our code telling us

--- subarray.java
+++ subarray.java
@@ -1,4 +1,36 @@
-public class subarray {
+
+
+/*print subarrays
+ if arr[] = {4,5,6,2,1}
+ then output:
+4 
+4 5 
+4 5 6 
+4 5 6 2 
+4 5 6 2 1 
+
+5 
+5 6 
+5 6 2 
+5 6 2 1 
+
+6 
+6 2 
+6 2 1 
+
+2 
+2 1 
+
+1 
+
+Total Subarray : 15
+ 
+
+and print total number of subarray ts
+Total subarrays  = (n*(n+1)) / 2; 
+ */
+
+ public class subarray {
     public static void printSubarray(int num[])
     {
         int ts=0;
@@ -11,7 +43,7 @@ public class subarray {
                 int end =j;
                 for(int k=start;k<=end;k++)
                 {
-                    System.out.print(num[k]+ ",");
+                    System.out.print(num[k]+ " ");
                 }
                 ts++;
                 System.out.println();
