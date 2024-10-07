0b949f1a4bf66c35b2ee5cb700afdd6f0f067040
OM CHOKSI <omchoksi108@gmail.com>
2024-10-07 09:13:24 +0530

    okay

--- sortingalgo.java
+++ sortingalgo.java
@@ -24,7 +24,18 @@ class sortingalgo{
     }
 
     public static void SelectionSort(int arr[]){
-
+            for(int i=0;i<arr.length-1;i++){
+                int minPose=i;
+                for(int j=i+1;j<arr.length;j++){
+                    if(arr[minPose]>arr[j]){
+                        minPose=j; 
+                    }
+                }
+                //swap
+                int temp=arr[minPose];
+                arr[minPose]=arr[i];
+                arr[i]=temp;
+            }
 
     }
 
@@ -34,8 +45,9 @@ class sortingalgo{
         }
     }
     public static void main(String args[]){
-           int arr[] = {5,4,3,28,2};
-           BubbleSort(arr);
+           int arr[] = {5,4,1,3,2};
+           //BubbleSort(arr);
+           SelectionSort(arr);
            PrintArray(arr);
     }
 
