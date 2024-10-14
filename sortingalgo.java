503532d3bd318a6d3ff89816be5edebac10ef7be
OM CHOKSI <omchoksi108@gmail.com>
2024-10-14 18:43:36 +0530

    changes

--- sortingalgo.java
+++ sortingalgo.java
@@ -1,3 +1,8 @@
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 //File is made by Om Choksi 23aiml010
 //code is for sorting algorithm 
 //BubbleSort
@@ -22,7 +27,20 @@ class sortingalgo{
 
          }
     }
-
+    
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr =arr[i];
+            int prev=i-1;
+            //finding out the correct pos
+            while(prev>=0 && arr[prev]>curr){
+              arr[prev+1] = arr[prev]; 
+              prev--;    
+            }
+            //insertion
+            arr[prev+1]=curr;
+        }
+    }
     public static void SelectionSort(int arr[]){
             for(int i=0;i<arr.length-1;i++){
                 int minPose=i;
@@ -31,7 +49,7 @@ class sortingalgo{
                         minPose=j; 
                     }
                 }
-                //swap
+                //swap in outer loop
                 int temp=arr[minPose];
                 arr[minPose]=arr[i];
                 arr[i]=temp;
@@ -46,9 +64,15 @@ class sortingalgo{
     }
     public static void main(String args[]){
            int arr[] = {5,4,1,3,2};
-           //BubbleSort(arr);
-           SelectionSort(arr);
-           PrintArray(arr);
-    }
+         //BubbleSort(arr);
+         //SelectionSort(arr);
+         //insertionSort(arr);  
+         //PrintArray(arr);
+         //Arrays.sort(arr);
+         Arrays.sort(arr,0,5);
+         Arrays.sort(arr,0,5,Collections.reverseOrder())
+         PrintArray(arr);
+         
+        }
 
 }
